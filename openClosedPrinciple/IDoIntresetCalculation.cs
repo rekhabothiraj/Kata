2ee48c353db8650fdd060dd1e23c9258eabc9535
Rekha <rekha.bothiraj@bglgroup.co.uk>
2014-03-19 15:10:35 +0000

    OP

---
+++ openClosedPrinciple/IDoIntresetCalculation.cs
@@ -0,0 +1,7 @@
+﻿namespace ClassLibrary1
+{
+    public interface IDoIntresetCalculation
+    {
+        string CalculateIntrest();
+    }
+}
---
+++ openClosedPrinciple/IntrestCalculator.cs
@@ -0,0 +1,27 @@
+﻿namespace ClassLibrary1
+{
+    public class IntrestCalculator : IDoIntresetCalculation
+    {
+        private readonly string _intrest;
+
+        public IntrestCalculator(string intrest)
+        {
+            _intrest = intrest;
+        }
+
+        public IntrestCalculator()
+        {
+        }
+
+        public string CalculateIntrest()
+        {
+            if(_intrest == "SI")
+                return "SI";
+
+            if (_intrest == "CI")
+                return "CI";
+
+            return "";
+        }
+    }
+}
